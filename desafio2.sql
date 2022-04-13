a5ba1d1909f534a18e72c70dc13b35d62e117592
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-04-13 19:27:40 -0300

    refatorando desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
-SELECT COUNT(DISTINCT songs.song_name) AS cancoes,
-COUNT(DISTINCT artists.artist_name) AS artistas,
-COUNT(DISTINCT albuns.album_name) AS albuns
-FROM SpotifyClone.songs AS songs
-INNER JOIN SpotifyClone.artists AS artists
-INNER JOIN SpotifyClone.albuns AS albuns;
+SELECT COUNT(DISTINCT s.song_name) AS cancoes,
+COUNT(DISTINCT ar.artist_name) AS artistas,
+COUNT(DISTINCT al.album_name) AS albuns
+FROM SpotifyClone.songs AS s
+INNER JOIN SpotifyClone.artists AS ar
+INNER JOIN SpotifyClone.albuns AS al;
