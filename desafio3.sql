d4640ce2b4393b8b08f423d98f2e7f564262293c
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-04-14 10:12:29 -0300

    3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT u.user_name AS usuario,
+COUNT(h.song_id) AS qtde_musicas_ouvidas,
+ROUND(SUM(s.song_length) / 60, 2) AS total_minutos
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.historical AS h
+ON u.user_id = h.user_id
+INNER JOIN SpotifyClone.songs AS s
+ON s.song_id = h.song_id
+GROUP BY u.user_name
+ORDER BY u.user_name;
